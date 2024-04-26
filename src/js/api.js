f19a19fb90c0344d7c7770f23c43c096adcfd115
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-04-26 12:41:41 +0300

    create function getApi

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,20 @@
 // export const api;
+
+import axios from 'axios';
+
+const baseUrlReviews = "https://portfolio-js.b.goit.study/api/reviews";
+
+async function getApi() {
+    const { data } = await axios.get(`${baseUrlReviews}`, {
+    headers: {
+        Accept: "application/json"
+    },
+    method: "Get"
+    })
+    console.log(data);
+    return data;
+
+    
+}
+
+getApi()
