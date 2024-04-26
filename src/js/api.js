648c38b5dcb12aa319b3e9bebb9b7c4382218fa3
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-04-26 13:26:25 +0300

    create function createMarkup and addCardsOnPage

--- src/js/api.js
+++ src/js/api.js
@@ -4,14 +4,13 @@ import axios from 'axios';
 
 const baseUrlReviews = "https://portfolio-js.b.goit.study/api/reviews";
 
-async function getApi() {
+ export async function getApi() {
     const { data } = await axios.get(`${baseUrlReviews}`, {
     headers: {
         Accept: "application/json"
     },
     method: "Get"
     })
-    console.log(data);
     return data;
 
     
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,4 +1,31 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 
-// import api from './api';
+import {getApi} from './api';
+
+const list = document.querySelector(".reviews_cards");
+
+async function addCardsOnPage() {
+    try {
+     const data = await getApi();
+     list.insertAdjacentHTML("beforeend", createMarkup(data));
+    }
+    catch (error) {
+        console.log(error.message);
+        
+    }
+    
+}
+ 
+function createMarkup(arr) {
+    return arr.map(({ id, author, avatar_url, review }) => `
+    <li class="card" data-id ="${id}">
+    <img class="card_img" src="${avatar_url}" alt="photo">
+    <h3 class="card_title">${author}</h3>
+    <p class="card_text">${review}</p> 
+    </li>
+    </ul>`)
+        .join("") 
+}
+
+addCardsOnPage()
