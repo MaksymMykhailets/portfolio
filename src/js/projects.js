501330728b309883be8d0b1e4d760841c52833c8
Oleksandra <sashakirina@gmail.com>
2024-04-27 15:30:05 +0200

    html update

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,7 +3,28 @@ import 'swiper/css';
 
 const swiperEl = document.querySelector('.swiper')
 
-const swiper = new Swiper(swiperEl);
+const swiper = new Swiper(swiperEl, {
+  // configuration options
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    slideChange: function () {
+      if (swiper.isEnd) {
+        swiper.navigation.next.disable();
+      } else {
+        swiper.navigation.next.enable();
+      }
+
+      if (swiper.isBeginning) {
+        swiper.navigation.prev.disable();
+      } else {
+        swiper.navigation.prev.enable();
+      }
+    },
+  },
+});
 
   console.log(swiper);
 
