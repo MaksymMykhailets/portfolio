1bdbcdb238bb60e85bd0cabeaf174939c709ccc2
Yuliia7777 <yuliiakorobkova1@gmail.com>
2024-04-28 13:52:02 +0200

    accordion

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,20 +3,19 @@ import Accordion from 'accordion-js';
 import Swiper from 'swiper';
 import 'swiper/css';
 import '../css/about-me.css';
-const accordions = Array.from(
-    document.querySelectorAll('.accordion-container')
-  );
-  new Accordion(accordions, {
-    duration: 400,
-    openOnInit: [0],
-    showMultiple: true,
-    onOpen: function (currentElement) {
-      let use = currentElement.querySelector('use[href]');
-      use.setAttribute('href', '../img/icons.svg#icon-arrow-up');
-    },
-    onClose: function (currentElement) {
-      let use = currentElement.querySelector('use[href]');
-      use.setAttribute('href', '../img/icons.svg#icon-arrow-down');
-    },
-  });
-  
+document.addEventListener("DOMContentLoaded", function() {
+    const accordions = Array.from(document.querySelectorAll('.accordion-container'));
+    new Accordion(accordions, {
+        duration: 400,
+        openOnInit: 'all',
+        showMultiple: true,
+        onOpen: function (currentElement) {
+            let use = currentElement.querySelector('use[href]');
+            use.setAttribute('href', '../img/icons.svg#icon-arrow-up');
+        },
+        onClose: function (currentElement) {
+            let use = currentElement.querySelector('use[href]');
+            use.setAttribute('href', '../img/icons.svg#icon-arrow-down');
+        },
+    });
+});
