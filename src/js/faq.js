b3e11b63cde8dfbc16bb007e33f32496c6869f41
Olena457 <olenademianenko42@gmail.com>
2024-04-30 00:04:49 +0300

    created faq js

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,2 +1,37 @@
-//import Accordion from 'accordion-js';//
-//import 'accordion-js/dist/accordion.min.css';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordion = new Accordion('.accordion', {
+    duration: 600,
+    showMultiple: false,
+  });
+
+  const accButtons = document.querySelectorAll('.ac-expand');
+
+  accButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      const acItem = btn.closest('.ac-item');
+
+      if (acItem.classList.contains('open')) {
+        acItem.classList.remove('open');
+      } else {
+        document.querySelectorAll('.ac-item.open').forEach(activeItem => {
+          activeItem.classList.remove('open');
+        });
+        acItem.classList.add('open');
+      }
+    });
+  });
+
+  document.querySelectorAll('.ac-arrow').forEach(button => {
+    button.addEventListener('click', () => {
+      button.classList.toggle('up');
+      button.classList.toggle('down');
+    });
+  });
+
+  document.querySelectorAll('.ac-item.open').forEach(activeItem => {
+    activeItem.classList.remove('open');
+  });
+});
