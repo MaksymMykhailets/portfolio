602348e3392d75a26cb52d31d6147e662629eb59
Volodymyr <palcinskijvolodimir173@gmail.com>
2024-04-30 15:37:23 +0300

    added tags to move and added function to close menu with escape button

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,9 +2,11 @@ const openModalBtn = document.querySelector('.open-modal-btn');
 const closeModalBtn = document.querySelector('.close-modal-btn');
 const modalListItem = document.querySelectorAll('.modal-item-link');
 const modalCont = document.querySelector('.modal-window-cont');
+const orderModalBtn = document.querySelector('.menu-order-btn');
 
 openModalBtn.addEventListener('click', toggleMenu);
 closeModalBtn.addEventListener('click', toggleMenu);
+orderModalBtn.addEventListener('click', toggleMenu);
 modalListItem.forEach(item => {
   item.addEventListener('click', toggleMenu);
 });
@@ -13,3 +15,8 @@ function toggleMenu() {
   modalCont.classList.toggle('is-open');
   document.body.classList.toggle('menu-is-open');
 }
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    modalCont.classList.remove('is-open');
+  }
+});
